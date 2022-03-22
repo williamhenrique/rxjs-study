87b1018ec3a69f006af0592043ae226905d4380c
williamhenrique <william.henrique@live.com>
2022-03-21 21:48:51 -0300

    1 file modified

--- share.ts
+++ share.ts
@@ -1,5 +1,14 @@
-import { interval } from 'rxjs';
+import { interval, map, share } from 'rxjs';
 
 export default () => {
-  interval(1000).subscribe(console.log);
+  const source$ = interval(1000).pipe(
+    map((x: number) => {
+      console.log('Processing: ', x);
+      return x * x;
+    }),
+    share()
+  );
+
+  source$.subscribe((x) => console.log('subscription 1: ', x));
+  source$.subscribe((x) => console.log('subscription 1: ', x));
 };
